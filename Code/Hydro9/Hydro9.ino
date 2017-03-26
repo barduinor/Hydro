c6ee1fd73a854c5aa603c5841b681508c31c506e
Barduino <barduinor@gmail.com>
2017-03-26 17:34:57 -0400

    Upgrade to MySensor 2.11 & IDE 1.8.1
    Something strange in Time.h needed to include the timeLib.h

--- Code/Hydro9/Hydro9.ino
+++ Code/Hydro9/Hydro9.ino
@@ -1,7 +1,25 @@
 /**
+   The MySensors Arduino library handles the wireless radio link and protocol
+   between your home built sensors/actuators and HA controller of choice.
+   The sensors forms a self healing radio network with optional repeaters. Each
+   repeater and gateway builds a routing tables in EEPROM which keeps track of the
+   network topology allowing messages to be routed to nodes.
 
+   Documentation: http://www.mysensors.org
+   Support Forum: http://forum.mysensors.org
 
-**/
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   version 2 as published by the Free Software Foundation.
+
+ *******************************
+
+   REVISION HISTORY
+   Version 2.0 - Barduino
+
+   DESCRIPTION
+  Water pump operations
+*/
 
 // Enable debug prints to serial monitor
 #define MY_DEBUG
@@ -14,13 +32,14 @@
 //#define MY_REPEATER_FEATURE
 
 // Define Node ID
-#define MY_NODE_ID 7
+#define MY_NODE_ID 30 // 30, 31,..., sun room
 
 // INCLUDES //
 #include <SPI.h>
-#include <MySensor.h>
+#include <MySensors.h>
 #include <Bounce2.h>
 #include <Time.h>
+#include <TimeLib.h>
 #include <DS1302RTC.h>  // A  DS3231/DS3232 library
 #include <DHT.h> // DHT library
 #include <DallasTemperature.h>
@@ -32,42 +51,42 @@
 
 // Skecth name and version
 #define SKETCH_NAME "Hydro9"
-#define SKETCH_VERSION "v0.1"
+#define SKETCH_VERSION "v1.999"
 
 
 Bounce debouncer = Bounce();
 int oldValue = 0;
 unsigned long lastUpdate = 0;
 
-unsigned long light_last_update=0;
+unsigned long light_last_update = 0;
 
 //present(MY_PUMP_ID, S_BINARY, "Pump Switch");
-  MyMessage msgPump(MY_PUMP_ID, V_STATUS);
-  
+MyMessage msgPump(MY_PUMP_ID, V_STATUS);
+
 //present(MY_PUMPMODE_ID, S_SCENE_CONTROLLER, "Pump Mode");
-  MyMessage msgPumpMode(MY_PUMPMODE_ID,V_SCENE_ON);
-  
+MyMessage msgPumpMode(MY_PUMPMODE_ID, V_SCENE_ON);
+
 //present(MY_SCHEDULER_ID, S_CUSTOM, "Pump  Scheduler");
-  MyMessage msgScheduleStart(MY_SCHEDULER_ID,V_VAR1);
-  MyMessage msgScheduleStop(MY_SCHEDULER_ID,V_VAR2);
-  
+MyMessage msgScheduleStart(MY_SCHEDULER_ID, V_VAR1);
+MyMessage msgScheduleStop(MY_SCHEDULER_ID, V_VAR2);
+
 //present(MY_DAYLIGHT_ID, S_CUSTOM, "Pump Daylight");
-  MyMessage msgLight(MY_DAYLIGHT_ID, V_LEVEL);
-  MyMessage msgLightNow(MY_DAYLIGHT_ID, V_VOLUME);
+MyMessage msgLight(MY_DAYLIGHT_ID, V_LEVEL);
+MyMessage msgLightNow(MY_DAYLIGHT_ID, V_VOLUME);
 
 //present(MY_PUMPTIME_ID, S_CUSTOM, "Pump Time");
-  MyMessage msgTime(MY_PUMPTIME_ID,V_VAR1);
-  
+MyMessage msgTime(MY_PUMPTIME_ID, V_VAR1);
+
 //present(MY_PUMPCYCLE_ID, S_CUSTOM, "Pump Cycle");
-  MyMessage msgCycleOn(MY_PUMPCYCLE_ID,V_LEVEL);
-  MyMessage msgCycleOff(MY_PUMPCYCLE_ID,V_VOLUME);
-  
+MyMessage msgCycleOn(MY_PUMPCYCLE_ID, V_LEVEL);
+MyMessage msgCycleOff(MY_PUMPCYCLE_ID, V_VOLUME);
+
 //present(MY_AIRSENSOR_ID, S_TEMP, "Air Sensor");
-  MyMessage msgAirTemp(MY_AIRSENSOR_ID,V_TEMP);
-  MyMessage msgAirHum(MY_AIRSENSOR_ID,V_HUM);
-  
+MyMessage msgAirTemp(MY_AIRSENSOR_ID, V_TEMP);
+MyMessage msgAirHum(MY_AIRSENSOR_ID, V_HUM);
+
 //present(MY_WATERSENSOR_ID, S_TEMP, "Water Sensor");
-  MyMessage msgWaterTemp(MY_WATERSENSOR_ID,V_TEMP);
+MyMessage msgWaterTemp(MY_WATERSENSOR_ID, V_TEMP);
 
 MyPump myPump(MY_PUMP_RELAY_PIN);
 MyEnvironment myEnvironment;
@@ -78,7 +97,7 @@ void setup()
   myEnvironment.dhtInit();
   myEnvironment.dallasInit();
   myPump.rtc_init();
-  
+
   // ********************* HARDWHERE *****************
   // Setup the button
   pinMode(BUTTON_PIN, INPUT);
@@ -88,27 +107,27 @@ void setup()
   // After setting up the button, setup debouncer
   debouncer.attach(BUTTON_PIN);
   debouncer.interval(5);
-  
-  
-  
+
+
+
   // Request latest time from controller at startup
-  
+
   requestTime();
   wait(2000);// Wait for time from controller
-  
-  myPump.pumpScheduleStart(7,33);
-  myPump.pumpScheduleStop(17,30);
-  
+
+  myPump.pumpScheduleStart(7, 33);
+  myPump.pumpScheduleStop(17, 30);
+
   myPump.pumpLuxStart(70);
-  
+
   myPump.mode(RUN_MODE_DAYLIGHT);
-  
-  //reportStatus();  
+
+  //reportStatus();
 }
 
 void presentation()  {
   Serial.println("Starting presentation");
-  
+
   // Send the sketch version information to the gateway and Controller
   sendSketchInfo(SKETCH_NAME, SKETCH_VERSION);
 
@@ -121,7 +140,7 @@ void presentation()  {
   present(MY_AIRSENSOR_ID, S_TEMP, "Air Sensor");
   present(MY_WATERSENSOR_ID, S_TEMP, "Water Sensor");
   present(MY_PUMPTIME_ID, S_CUSTOM, "Pump Time");
-  
+
   Serial.println("End presentation");
 }
 
@@ -135,18 +154,18 @@ void loop()
     send(msgPump.set(myPump.pumpSwitch()), false);
   }
   oldValue = value;
-  
-  if(myPump.pumpCheck() || myEnvironment.check())
+
+  if (myPump.pumpCheck() || myEnvironment.check())
   {
-     reportStatus();
+    reportStatus();
   }
-  
-  if (((light_last_update + (15L * 60L * 1000UL)) < millis()) or (light_last_update == 0)){
+
+  if (((light_last_update + (15L * 60L * 1000UL)) < millis()) or (light_last_update == 0)) {
     Serial.print(myPump.currentDateTime().c_str());
     Serial.print("Light level: ");
     Serial.println(myPump.currentLuxLevel());
     light_last_update = millis();
-    
+
     reportStatus();
   }
 }
@@ -158,79 +177,79 @@ void receive(const MyMessage &message) {
     Serial.println("This is an ack from gateway");
   }
 
-  switch(message.sensor){
+  switch (message.sensor) {
     case MY_PUMP_ID:      if (message.type == V_STATUS) {
-                            if (message.getBool())
-                              myPump.pumpOn();
-                            else
-                              myPump.pumpOff();
-                          }
-                          break;
-                          
+        if (message.getBool())
+          myPump.pumpOn();
+        else
+          myPump.pumpOff();
+      }
+      break;
+
     case MY_PUMPCYCLE_ID: if (message.type == V_LEVEL) {
-                            myPump.pumpCycleRun(message.getInt());
-                          } 
-                          if (message.type == V_VOLUME) {
-                            myPump.pumpCycleStop(message.getInt());
-                          } 
-                          break;
-                          
+        myPump.pumpCycleRun(message.getInt());
+      }
+      if (message.type == V_VOLUME) {
+        myPump.pumpCycleStop(message.getInt());
+      }
+      break;
+
     case MY_PUMPMODE_ID:  if (message.type == V_SCENE_ON) {
-                            myPump.mode((pump_run_mode)message.getInt());
-                          } 
-                          break;
-                          
-    case MY_SCHEDULER_ID: 
-                          if (message.type == V_VAR1) {
-                            myPump.pumpScheduleStart((byte)myPayload.substring(0,2).toInt(),(byte)myPayload.substring(3,5).toInt()) ;   
-                          } 
-                          if (message.type == V_VAR2) {
-                            myPump.pumpScheduleStop((byte)myPayload.substring(0,2).toInt(),(byte)myPayload.substring(3,5).toInt()) ;   
-                          } 
-                          break;
-                          
+        myPump.mode((pump_run_mode)message.getInt());
+      }
+      break;
+
+    case MY_SCHEDULER_ID:
+      if (message.type == V_VAR1) {
+        myPump.pumpScheduleStart((byte)myPayload.substring(0, 2).toInt(), (byte)myPayload.substring(3, 5).toInt()) ;
+      }
+      if (message.type == V_VAR2) {
+        myPump.pumpScheduleStop((byte)myPayload.substring(0, 2).toInt(), (byte)myPayload.substring(3, 5).toInt()) ;
+      }
+      break;
+
     case MY_DAYLIGHT_ID:  if (message.type == V_LEVEL) {
-                            myPump.pumpLuxStart(message.getInt());
-                          } 
-                          
-                          break;
-    
+        myPump.pumpLuxStart(message.getInt());
+      }
+
+      break;
+
     default:  break;
   }
 
-//  //debug message
-//  Serial.println();
-//  Serial.print("********************************");
-//  Serial.println();
-//  Serial.print("Get String:");
-//  Serial.print(message.getString());
-//  Serial.println();
-//  Serial.print("Get Boolean:");
-//  Serial.print(message.getBool());
-//  Serial.println();
-//  Serial.print("Get Byte:");
-//  Serial.print(message.getByte());
-//  Serial.println();
-//  Serial.print("Get Float:");
-//  Serial.print(message.getFloat());
-//  Serial.println();
-//  Serial.print("Get Int:");
-//  Serial.print(message.getInt());
-//  Serial.println();
-//  Serial.print("Get UInt:");
-//  Serial.print(message.getUInt());
-//  Serial.println();
-//  Serial.print("Get Long:");
-//  Serial.print(message.getLong());
-//  Serial.println();
-//  Serial.print("Get ULong:");
-//  Serial.print(message.getULong());
-//  Serial.println();
-//  Serial.print("Get Command:");
-//  Serial.print(message.getCommand());
-//  Serial.println();
-//  Serial.print("********************************");
-//  Serial.println();
+  //  //debug message
+  //  Serial.println();
+  //  Serial.print("********************************");
+  //  Serial.println();
+  //  Serial.print("Get String:");
+  //  Serial.print(message.getString());
+  //  Serial.println();
+  //  Serial.print("Get Boolean:");
+  //  Serial.print(message.getBool());
+  //  Serial.println();
+  //  Serial.print("Get Byte:");
+  //  Serial.print(message.getByte());
+  //  Serial.println();
+  //  Serial.print("Get Float:");
+  //  Serial.print(message.getFloat());
+  //  Serial.println();
+  //  Serial.print("Get Int:");
+  //  Serial.print(message.getInt());
+  //  Serial.println();
+  //  Serial.print("Get UInt:");
+  //  Serial.print(message.getUInt());
+  //  Serial.println();
+  //  Serial.print("Get Long:");
+  //  Serial.print(message.getLong());
+  //  Serial.println();
+  //  Serial.print("Get ULong:");
+  //  Serial.print(message.getULong());
+  //  Serial.println();
+  //  Serial.print("Get Command:");
+  //  Serial.print(message.getCommand());
+  //  Serial.println();
+  //  Serial.print("********************************");
+  //  Serial.println();
   reportStatus();
 }
 
@@ -239,29 +258,29 @@ void receiveTime(unsigned long controllerTime)
   myPump.rtc_set(controllerTime);
 }
 
-void reportStatus(){
+void reportStatus() {
   myPump.pumpStatus();
   myEnvironment.status();
-  
+
   send(msgPump.set(myPump.isOn()), false);
-  
-  send(msgPumpMode.set(myPump.mode()),false);
-  
-  send(msgScheduleStart.set(myPump.pumpScheduleStart().c_str()),false);
-  send(msgScheduleStop.set(myPump.pumpScheduleStop().c_str()),false);
-  
-  send(msgLight.set(myPump.pumpLuxStart()),false);
+
+  send(msgPumpMode.set(myPump.mode()), false);
+
+  send(msgScheduleStart.set(myPump.pumpScheduleStart().c_str()), false);
+  send(msgScheduleStop.set(myPump.pumpScheduleStop().c_str()), false);
+
+  send(msgLight.set(myPump.pumpLuxStart()), false);
   send(msgLightNow.set(myPump.currentLuxLevel()), false);
-  
+
   send(msgCycleOn.set(myPump.pumpCycleRun()), false);
   send(msgCycleOff.set(myPump.pumpCycleStop()), false);
-  
+
   send(msgTime.set(myPump.currentDateTime().c_str()), false);
-  
-  send(msgAirTemp.set(myEnvironment.getAirTemp(),1), false);
-  send(msgAirHum.set(myEnvironment.getAirHum(),1), false);
-  
-  send(msgWaterTemp.set(myEnvironment.getWaterTemp(),1), false);
+
+  send(msgAirTemp.set(myEnvironment.getAirTemp(), 1), false);
+  send(msgAirHum.set(myEnvironment.getAirHum(), 1), false);
+
+  send(msgWaterTemp.set(myEnvironment.getWaterTemp(), 1), false);
 
 }
 
